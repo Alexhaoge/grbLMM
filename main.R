b032a2414c8899563a0506cc7f1a5f28abde7527
Zhihao Lyu <alexhaoge@gmail.com>
2024-03-05 12:54:01 -0800

    add decision tree wrappers

--- main.R
+++ main.R
@@ -1,13 +1,42 @@
+library(rpart)
+library(glmnet)
+
 source("R/grbLMM.R")
 
 # Define wrapper for decision tree
-dt.init = NULL
+dt.init = list(lr=list(), model=list())
 dt.predict = function(dt, X) {
-
+  if (is.null(dt)) {
+    rep(0, nrow(X))
+  } else {
+    yhat = rep(0, nrow(X))
+    for (i in seq_along(dt$model)) {
+      yhat = yhat + dt$lr[i] * predict(dt$model[i], newdata = as.data.frame(X))
+    }
+    yhat
+  }
 }
 dt.fit = function(dt, X, y, lr){
-
+  if (is.null(dt)) {
+    dt = list(lr=list(), model=list())
+  }
+  dt$lr = append(dt$lr, lr)
+  ds = as.data.frame(X)
+  ds$y = y
+  dt$model = append(dt$model, rpart(y~., ds))
+  dt
 }
+
+# Define wrapper for lasso
+# lasso.init = NULL
+# lasso.predict = NULL
+# lasso.predict = function(beta, X) {
+#   cbind(1, X) %*% beta
+# }
+# lasso.fit = function(beta, X, y, lr, family = "Gaussian", intercept = TRUE, lambda = NULL){
+#   lasso = glmnet(X, y, family = family, alpha = 1, intercept = intercept, lambda = lambda)
+
+# }
 
 # load data
 
